e2534bd7595f91cf5cc4084983be37cbdd1d137f
LBBO <mdk@kuckuk.com>
2015-01-28 19:04:22 +0100

    Update captcha.inc.php
    unnessecary ? true:false deleted twice

--- captcha.inc.php
+++ captcha.inc.php
@@ -28,9 +28,9 @@ class simple_captcha {
 	private function is_session_started() {
 		if(php_sapi_name() !== 'cli') {
 			if(version_compare(phpversion(), '5.4.0', '>=')) {
-				return session_status() === PHP_SESSION_ACTIVE ? true : false;
+				return session_status() === PHP_SESSION_ACTIVE;
 			} else {
-				return session_id() === '' ? false : true;
+				return session_id() === '';
 			}
 		}
 		return false;
